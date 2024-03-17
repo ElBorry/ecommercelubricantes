b6141747ad9262a768bdad446db9a91d5a4bc610
EjsmUIUX <ejsm1623@gmail.com>
2024-03-17 00:50:08 -0500

    Correciones del Desafio 1.

--- userManager.js
+++ userManager.js
@@ -1,41 +1,38 @@
-//Creación de la Clase UserManager
 class UserManager {
-    // Se definen los parametros de creación de la clase.
-    constructor() {
-      this._users = [];
-      this._nextUserId = 1;
-    }
-  
-    //Metodo de adición o creación de usuarios mediante el parametro de data
-    createUser(data) {
+  // Variables privadas y estáticas
+  static #users = [];
+  static #nextUserId = 1;
+
+  // Método de adición o creación de usuarios mediante el parámetro de data
+  static createUser(data) {
       const user = {
-          id: this._nextUserId++,
+          id: UserManager.#nextUserId++,
           photo: data.photo,
           email: data.email,
           password: data.password,
           role: data.role,
       }
-      this._users.push(user);
-    }
-  
-    //Metodo para la lectura de los usuarios dentro de la classe UserManager
-    readUsers() {
-      return this._users
-    }
+      UserManager.#users.push(user);
   }
-  
-  const userManager = new UserManager();
-  userManager.createUser({
-    photo: "ruta_imagen_1",
-    email: "usuario1@example.com",
-    password: "contraseña1",
-    role: "rol1",
-  });
-  userManager.createUser({
-    photo: "ruta_imagen_2",
-    email: "usuario2@example.com",
-    password: "contraseña2",
-    role: "rol2",
-  });
-  // Ejecución del metodo de lectura
-  console.log(userManager.readUsers());
+
+  // Método para la lectura de los usuarios dentro de la clase UserManager
+  static readUsers() {
+      return UserManager.#users;
+  }
+}
+
+UserManager.createUser({
+  photo: "ruta_imagen_1",
+  email: "usuario1@example.com",
+  password: "contraseña1",
+  role: "rol1",
+});
+UserManager.createUser({
+  photo: "ruta_imagen_2",
+  email: "usuario2@example.com",
+  password: "contraseña2",
+  role: "rol2",
+});
+
+// Ejecución del método de lectura
+console.log(UserManager.readUsers());
