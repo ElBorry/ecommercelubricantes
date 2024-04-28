993301b45de605602fc702b1507c255f35d21ad8
ElBorry <borriello1985@gmail.com>
2024-04-28 08:46:12 -0300

    Eliminación de archivos y directorios innecesarios

--- src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-// Archivo: src/middlewares/errorHandler.js
-
-export default function errorHandler(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Algo salió mal en el servidor' });
-  }
-  
--- src/middlewares/notFoundHandler.js
+++
@@ -1,8 +0,0 @@
-// Ubicación: src/middlewares/notFoundHandler.js
-
-function notFoundHandler(req, res, next) {
-    res.status(404).send("Lo siento, no podemos encontrar eso!");
-  }
-  
-  export default notFoundHandler;
-  
